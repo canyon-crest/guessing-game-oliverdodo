ebfc39296fcec9160c1a729e1f43dbbca884904b
oliverdodo <doo7049@my.sduhsd.net>
2026-04-07 22:28:29 +0000

    first message

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
 // add javascript here
+let guess = 0;
+let answer = 0;
+let guessCount = 0;
+let totalWins = 0;
+const score[]; 
+
+document.getElementById("playBtn").addEventListener
+("click",play)
+
+function play(){
+    let range = 0;
+    let levels = document.getElementsByName("level");
+    for(let i=0; i<levels.length; i++){
+        if(levels[i].checked){
+            range = parseInt(levels[i].value);
+        }
+        levels[i].disabled = true;
+    }
+    document.getElementById("msg").textContent = "Guess a number 1-" + Range;
+    answer = Math.floor(Math.random()*range)
+    guessCount=0;
+
+    guessBtn.disabled = false;
+    giveUpBtn.disabled = false;
+    playBtn.disabled = true;
+}
